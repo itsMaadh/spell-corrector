744fd90217f6327f2d802c5b2710709a250aded8
Eric Heng <3richeng@gmail.com>
2022-06-06 11:21:06 +0800

    corpus - exported filtered unique tokens to csv file

--- corpus.py
+++ corpus.py
@@ -7,34 +7,45 @@ from nltk.probability import FreqDist
 from nltk.tokenize import RegexpTokenizer
 import csv
 
-#%% Pre-processing - Stemming
-
-def process_pdf(path, save_to="corpus/default.csv"):
+#%%
+def process_pdf(path, save_to="corpus/dictonary.csv"):
     
     """
     Description: extract PDF file contents to produce the corpus data
     Parameters:
         - path (str): PDF directory
+        - save_to (str): exported CSV file directory
     Return:
-        - array (string) : unique words from PDF
+        - list (string) : filtered unique tokens from PDF
     """ 
     # ref https://vitalflux.com/python-extract-text-pdf-file-using-pdfminer/
     
-    text = extract_text(path)
-    tokenizer = RegexpTokenizer('\w+')
-    tokens = np.unique(tokenizer.tokenize(text))
-    freqdist = FreqDist(tokens)
+    text = extract_text(path) # high level api function - to extract pdf text using pdfminer
+    tokenizer = RegexpTokenizer('[A-z]\w+') # create a regex token object
+    tokens = tokenizer.tokenize(text) # tokenize the text
+    unique_tokens = np.unique(tokens) # extract the unique tokens as the vocab dictonary
+    freqdist = FreqDist(tokens) # get the frequency distribution of the corpus
     
-    tokens = [words for words in tokens if len(words) > 0 and freqdist[words] > 0]
+    # Remove text shorter than N and freq less than M
+    filtered_unique_tokens = [words for words in unique_tokens if len(words) > 3 and freqdist[words] > 10]
+    
+    # Exporting to CSV File
+    with open(save_to, 'w') as f:
+        write = csv.writer(f)
+        write.writerow(filtered_unique_tokens)
+    
+    # Provide a simple statistics of the PDF   
+    print(f"Number of Words in PDF               : {len(text)}")
+    print(f"Number of tokens                     : {len(tokens)}")
+    print(f"Number of unique tokens              : {len(unique_tokens)}")
+    print(f"Number of filtered unique tokens     : {len(filtered_unique_tokens)}")
     print("Extract complete")
     
-    return tokens
+    return filtered_unique_tokens
 
 path = "corpus/an-introduction-to-business-v1.0.pdf"
 pdf_conent = process_pdf(path)
-
-len(np.unique(pdf_conent))
-#%% Pre-processing - Stemming
+#%%
 
 def stemming(data, stemmer):
     """
@@ -46,6 +57,4 @@ def stemming(data, stemmer):
         - list/array of stemmed text
     """
     return [stemmer.stem(t) for t in data]
-
-#%% Pre-processing - Stemming
 
