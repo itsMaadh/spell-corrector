b0021dbaf8c8382def557bb6661dfa4831b7a705
Eric Heng <3richeng@gmail.com>
2022-06-06 10:24:28 +0800

    pdf text preprocessing - tokenization

--- corpus.py
+++ corpus.py
@@ -1,61 +1,51 @@
 # Precondition
-import nltk
-from nltk.corpus import gutenberg
-import PyPDF2 
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
+# https://vitalflux.com/python-extract-text-pdf-file-using-pdfminer/
 
-#%% Extract Content from PDF
-page = ""
-# creating a pdf file object 
-pdfFileObj = open('corpus/dictionary.pdf', 'rb') 
+import numpy as np
+from pdfminer.high_level import extract_text
+from nltk.probability import FreqDist
+from nltk.tokenize import RegexpTokenizer
+import csv
+
+#%% Pre-processing - Stemming
+
+def process_pdf(path, save_to="corpus/default.csv"):
     
-# creating a pdf reader object 
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
+    """
+    Description: extract PDF file contents to produce the corpus data
+    Parameters:
+        - path (str): PDF directory
+    Return:
+        - array (string) : unique words from PDF
+    """ 
+    # ref https://vitalflux.com/python-extract-text-pdf-file-using-pdfminer/
     
-numOfPages = pdfReader.numPages
+    text = extract_text(path)
+    tokenizer = RegexpTokenizer('\w+')
+    tokens = np.unique(tokenizer.tokenize(text))
+    freqdist = FreqDist(tokens)
+    
+    tokens = [words for words in tokens if len(words) > 0 and freqdist[words] > 0]
+    print("Extract complete")
+    
+    return tokens
 
-for i in range (0, numOfPages):
-    print(i)
-    pageObj = pdfReader.getPage(10)
-    page += pageObj.extractText()
+path = "corpus/an-introduction-to-business-v1.0.pdf"
+pdf_conent = process_pdf(path)
 
-pdfFileObj.close() 
+len(np.unique(pdf_conent))
+#%% Pre-processing - Stemming
 
-#%% Extract Content from PDF
-text_file = open("dic.txt", "w")
-n = text_file.write(page)
-text_file.close()
+def stemming(data, stemmer):
+    """
+    Description: breaking the raw text into small chunks
+    Parameters:
+        - data: raw text data
+        - stemmer: nltk stemmer object
+    Return:
+        - list/array of stemmed text
+    """
+    return [stemmer.stem(t) for t in data]
 
-
-#%% Pre-processing - Remove punctuation / Remove extra white space
-import string
-text_p = ""
-
-text_p = "".join([char for char in page if char not in string.punctuation])
-print(text_p)
-
-def remove_whitespace(text):
-    return  " ".join(text.split())
-
-remove_whitespace(text_p)
-
-#%% Pre-processing - 
-import string
-
-text_p = "".join([char for char in page if char not in string.punctuation])
-print(text_p)
-
-
-#%% Pre-processing - Tokenization
-
-
-tokens = word_tokenize(page)
-
-porter = nltk.PorterStemmer()
-lancaster = nltk.LancasterStemmer()
-
-print("Porter:",[porter.stem(t) for t in tokens])
-print("\n")
-print("Lancaster:",[lancaster.stem(t) for t in tokens])
+#%% Pre-processing - Stemming
 
